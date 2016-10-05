c8fa0b0729c896f8c4b8c9e9c8d1ca610199672d
Jan Lelis <mail@janlelis.de>
2016-10-05 14:57:44 +0200

    Rename star spinner to starlike

--- lib/whirly/spinners/whirly.rb
+++ lib/whirly/spinners/whirly.rb
@@ -7,7 +7,7 @@ module Whirly
       "random_dots"         => { "proc" => ->(){ [ 0x2800 + rand(256)].pack("U") }, "interval" => 100 },
       "circled_letter"      => { "proc" => ->(){ [ 0x24B6 + rand(26) ].pack("U") }, "interval" => 120 },
       "circled_number"      => { "proc" => ->(){ [ 0x2460 + rand(9)  ].pack("U") }, "interval" => 120 },
-      "star"                => { "proc" => ->(){ [ 0x2729 + rand(34) ].pack("U") }, "interval" => 120 },
+      "starlike"            => { "proc" => ->(){ [ 0x2729 + rand(34) ].pack("U") }, "interval" => 120 },
       "mahjong"             => { "proc" => ->(){ [0x1F000 + rand(44)].pack("U") }, "interval" => 200 },
       "domino"              => { "proc" => ->(){ [0x1F030 + rand(50)].pack("U") }, "interval" => 200 },
       "vertical_domino"     => { "proc" => ->(){ [0x1F062 + rand(50)].pack("U") }, "interval" => 200 },
